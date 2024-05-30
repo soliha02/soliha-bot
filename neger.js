386e0dbd23422c715bab3250f532cc1518a9eb0f
soliha02 <164037162+soliha02@users.noreply.github.com>
2024-05-30 19:00:54 +0500

    Add files via upload

---
+++ neger.js
@@ -0,0 +1,48 @@
+
+
+let son = 'Sizga hozir random son chiqadi😎'
+
+
+
+alert( son)
+
+let sonlar = 'random son = 22🤗'
+
+alert(sonlar)
+
+let javob = '22 sonini 2 ga bolsak,11 soni chiqadi,keyin esa uni 3 ga kopaytirsak, 33 chiqadi.' 
+
+
+alert(javob)
+
+let xayir = "E'tiboringiz uchun raxmat!😍😉😙"
+
+alert(xayir)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let num1 = 3
+
+
+
+let num2 = 5
+
+console.log(num1 + num2);
